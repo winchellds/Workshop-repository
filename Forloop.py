f018c0e769af8af9264069b0a4c35478f6a22e1d
Winchell D'Souza <dsouza.winchell@gmail.com>
2016-01-30 16:27:15 -0700

    initial commit

---
+++ Forloop.py
@@ -0,0 +1,5 @@
+books = ["Tale of Two Cities", "Harry Potter", "To kill a mocking bird", "Famous five", "Batman"]
+nums = range(100)
+
+for book in books:
+    print(book)
---
+++ ifelse.py
@@ -0,0 +1,6 @@
+x = 7
+
+if x > 10:
+    print("That makes sense")
+else:
+    print("Won't work")
